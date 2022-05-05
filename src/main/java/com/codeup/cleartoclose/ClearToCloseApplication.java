09381fef424f86890df69cabb126c068722b350f
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-05-05 14:54:16 -0500

    first commit

---
+++ src/main/java/com/codeup/cleartoclose/ClearToCloseApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.cleartoclose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearToCloseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearToCloseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/cleartoclose/ClearToCloseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.cleartoclose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearToCloseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
