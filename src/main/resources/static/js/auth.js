b78e09b6e5f384bdfbd9dc2ce178a537f586d4d4
Scott Carpenter <carpenter.scott22@gmail.com>
2022-05-05 15:44:40 -0500

    initialized js folder w/ initial router, render, & createView

---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,76 @@
+import fetchData from "./fetchData.js";
+import createView from "./createView.js";
+
+/**
+ * Adds a login event to allow the user to initially obtain a new OAuth2.0 token
+ * On a successful response, sets the tokens into storage and redirects to the root
+ */
+export default function addLoginEvent() {
+    document.querySelector("#login-btn").addEventListener("click", function () {
+        let obj = {
+            username: document.querySelector("#username").value,
+            password: document.querySelector("#password").value,
+            grant_type: 'password'
+        }
+        // TODO: these are the only request params /oauth/token accepts in Spring Security
+        // TODO: need to possibly implement a random bit handshake w/ SHA256 on the password before sending
+        //      -> Alternatively, encrypt the entire request body
+        let request = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + btoa('rest-blog-client:secret')
+            },
+            body: `grant_type=${obj.grant_type}&username=${obj.username}&password=${obj.password}&client_id=rest-blog-client`
+        };
+
+        fetchData(
+            {
+                route: `/oauth/token`
+            },
+            request).then((data) => {
+            if (data.route.error === 'invalid_grant') {
+                $("#log-error-message").html("Invalid Credentials. Please Try Again")
+                return
+            }
+            setTokens(data);
+            createView("/");
+        });
+    });
+}
+
+/**
+ * Gets the Authorization header needed for making requests to protected endpoints
+ * This function should be used only after the user is logged in
+ * @returns {string}
+ */
+
+export function isLoggedIn() {
+    return localStorage.getItem("access_token")
+}
+
+export function getToken() {
+    return  'Bearer ' + `${localStorage.getItem("access_token")}`;
+}
+
+export function getHeaders() {
+    const token = localStorage.getItem("access_token");
+    return token
+        ? {
+            'Content-Type': "application/json",
+            'Authorization': 'Bearer ' + `${token}`}
+        : {'Content-Type': 'application/json'};
+}
+
+/**
+ * Attempts to set the access and refresh tokens needs to authenticate and authorize the client and user
+ * @param responseData
+ */
+function setTokens(responseData) {
+    if (responseData.route['access_token']) {
+        localStorage.setItem("access_token", responseData.route['access_token']);
+    }
+    if (responseData.route['refresh_token']) {
+        localStorage.setItem("refresh_token", responseData.route['refresh_token']);
+    }
+}
---
+++ src/main/resources/static/js/router.js
@@ -0,0 +1,27 @@
+
+
+
+/**
+ * Returns the route object for a specific route based on the given URI
+ * @param URI
+ * @returns {*}
+ */
+export default function router(URI) {
+    const routes = {
+        '/': {
+            returnView: Home,
+            state: {},
+            uri: '/',
+            title: 'Home',
+        },
+        '/login': {
+            returnView: Login,
+            state: {},
+            uri: '/login',
+            title: "Login",
+            viewEvent: LoginEvent
+        }
+    };
+
+    return routes[URI];
+}
