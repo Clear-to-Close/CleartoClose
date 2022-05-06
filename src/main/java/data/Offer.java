0a2f31792b4a94c913ac9a2684fc817ab6af6e94
Scott Carpenter <carpenter.scott22@gmail.com>
2022-05-06 13:55:44 -0500

    offer annotations added to properties

--- src/main/java/data/Offer.java
+++ src/main/java/data/Offer.java
@@ -1,19 +1,55 @@
 package data;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "offers")
 public class Offer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private long offerId;
+
+    // Foreign key pointing to User id
+    private long offererId;
+
+    // Foreign key pointing to listing table
     private long listingId;
+
+    @Column(nullable = false)
     private long offerAmount;
+
+    @Column(nullable = false)
     private String homeWarranty;
+
+    @Column(nullable = false)
     private long closingCosts;
+
+    @Column(nullable = false)
     private long optionLength;
+
+    @Column(nullable = false)
     private String appraisalWaiver;
+
+    @Column(nullable = false)
     private String survey;
+
+    // An associative table that lists different loan types may be desired here; would allow for a drop-down selection as well
+    @Column(nullable = false)
     private String loanType;
+
+    @Column(nullable = false)
     private String closingDate;
+
+    @Column
     private Date acceptanceDate;
+
 }
