e3416a8354fda73098433c6668b0f731421dc1f1
Trevor Esparza <trevoresparza30@gmail.com>
2022-05-06 08:33:09 -0500

    200, on to dynamic display

--- src/main/resources/static/js/views/listingView.js
+++ src/main/resources/static/js/views/listingView.js
@@ -1,39 +1,26 @@
 (function (){
 
-function requestListing(){
-    console.log('here here')
-    let url = "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?attomid=184713191";
+        var url = "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=78261&propertytype=RESIDENTIAL%20(NEC)&page=1&pagesize=10";
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
+        function requestListing() {
+            console.log('here here');
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
 
-    xhr.setRequestHeader("accept", "application/json");
-    xhr.setRequestHeader("apikey", "2b1e86b638620bf2404521e6e9e1b19e");
+            xhr.setRequestHeader("accept", "application/json");
+            xhr.setRequestHeader("apikey", "2b1e86b638620bf2404521e6e9e1b19e");
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-        }};
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    console.log(xhr.status);
+                    console.log(xhr.responseText);
+                }};
 
-    xhr.send();
+            xhr.send();
+        }
 
+        requestListing();
 
-}
+})();
 
-
-requestListing()
-
-
-}) ///END OF IIFE
-
-// $.get(url)
-//     .setRequestHeader("accept", "application/json")
-//     .setRequestHeader("apikey", `${ATTOM_API_KEY_CTC}`)
-//     .then(function (res){
-//         console.log(res)
-//         return res.json
-//     }).then(function (data){
-//     console.log(data)
-// })
 
