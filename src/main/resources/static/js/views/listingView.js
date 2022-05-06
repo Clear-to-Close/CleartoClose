aa7b583a452439f8e4e312a04a1de410627e782e
Trevor Esparza <trevoresparza30@gmail.com>
2022-05-06 09:15:24 -0500

    cleaned up line breaks us/14

--- src/main/resources/static/js/views/listingView.js
+++ src/main/resources/static/js/views/listingView.js
@@ -1,19 +1,18 @@
 (function (){
 
-        var url = "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=78261&propertytype=RESIDENTIAL%20(NEC)&page=1&pagesize=10";
+        var url = "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?attomid=164898522";
 
         function requestListing() {
-            console.log('here here');
             const xhr = new XMLHttpRequest();
             xhr.open("GET", url);
 
             xhr.setRequestHeader("accept", "application/json");
             xhr.setRequestHeader("apikey", "2b1e86b638620bf2404521e6e9e1b19e");
-
+            xhr.responseType = 'json';
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
                     console.log(xhr.status);
-                    console.log(xhr.responseText);
+                    console.log(xhr.response);
                 }};
 
             xhr.send();
