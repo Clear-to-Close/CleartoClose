9a04f77094f603964fa47c14636e2fcb84d090fe
Scott Carpenter <carpenter.scott22@gmail.com>
2022-05-07 10:14:30 -0500

    added acceptAnOffer post method in OffersController

--- src/main/java/web/OffersController.java
+++ src/main/java/web/OffersController.java
@@ -8,8 +8,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/offers", headers = "Accept=application/json")
 public class OffersController {
 
+    // Offer can be made, but authentication of the user needs to occur before US26/F2 is complete
     @PostMapping
     public void submitNewOffer(@RequestBody Offer newOffer) {
         System.out.printf("A new offer with the id of %d has been made!", newOffer.getId());
+    }
+
+    // Offer can be accepted upon, submit of a selection form; post updates the historical data of the selected offer
+    @PostMapping
+    public void acceptAnOffer(@RequestBody Offer acceptedOffer) {
+        System.out.printf("The seller has accepted an offer with the id of %d!", acceptedOffer.getId());
     }
 }
