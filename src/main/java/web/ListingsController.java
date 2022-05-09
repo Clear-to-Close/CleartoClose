e7c38efb4d80ea2488977092b7314d3f1ae0d031
Scott Carpenter <carpenter.scott22@gmail.com>
2022-05-09 09:38:18 -0500

    imported ListingsController to ListingsController and refactored methods to use the repository

--- src/main/java/web/ListingsController.java
+++ src/main/java/web/ListingsController.java
@@ -1,6 +1,7 @@
 package web;
 
 import data.Listing;
+import data.ListingRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,17 +11,23 @@ import java.util.List;
 @RequestMapping(value = "/api/listings", headers = "Accept=application/json")
 public class ListingsController {
 
+    private final ListingRepository listingRepository;
+
+    public ListingsController(ListingRepository listingRepository) {
+        this.listingRepository = listingRepository;
+    }
+
     // This would be more for user profile purposes; US22/F2? or US21/F3
     @GetMapping
     public List<Listing> getAllListings() {
-        return null;
+        return listingRepository.findAll();
     }
 
     // Will return a single listing with all information; US19/F2 As an auth user, I can see all listing information
     // TODO: Test by making listing object and return using this method
     @GetMapping("{listingId}")
     public Listing getListingById(@PathVariable Long listingId) {
-        return null;
+        return listingRepository.getById(listingId);
     }
 
 }
