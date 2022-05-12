899cbc4fc7d72f9fba41ff74766bf2e4a5d2fceb
Trevor Esparza <trevoresparza30@gmail.com>
2022-05-12 15:49:59 -0500

    fixed offers bug

--- src/main/resources/static/js/views/Offers.js
+++ src/main/resources/static/js/views/Offers.js
@@ -1,18 +1,23 @@
-import createView from "../createView.js";
-
 const OFFERS_URL = "http://localhost:8080/api/offers";
 
 export default function Offers(props) {
     console.log(props);
-    return `<div>${retrieveOffersFromDb(props.offers)};</div>`
-
+    return `<div>${retrieveOffersFromDb(props.offers)}</div>`
 }
 
 const retrieveOffersFromDb = (offers) => {
+    // language=HTML
     return offers.map(offer =>
-        `<div>class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id = "offerId-${offer.id}">${offer.id}
-
-</div>`
+        `
+			<div
+			<div class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id ="offerId">${offer.id}</div>
+			<div class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id ="offerAcceptance">${offer.acceptanceDate}</div>
+			<div class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id ="offerId">${offer.closingCosts}</div>
+			<div class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id ="offerId">${offer.closingDate}</div>
+			<div class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id ="offerId">${offer.homeWarranty}</div>
+			<div class="w-24 h-6 m-1 pb-1 border-2 rounded text-center" id ="offerId">${offer.loanType}</div>
+			
+			</div>`
     ).join("")
 };
 
