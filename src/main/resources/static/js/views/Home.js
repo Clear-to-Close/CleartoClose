af42be8a6888a00354201df46047348a0da0fd8b
Scott Carpenter <carpenter.scott22@gmail.com>
2022-05-13 12:39:19 -0500

    added input elements and styling to Home view

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -3,8 +3,25 @@ export default function Home() {
     return `
         <div class="bg-cover h-[calc(100vh-75px)] "
              style="background-image: url('https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')">
-            <div class="h-full w-full bg-gray-500 flex items-center justify-center bg-opacity-50">
-                <input type="text" name="search" id="searchListings" class="my-1 w-3/4" placeholder="Search For Your Dream Home">
+            <div class="h-full w-full flex items-center justify-center bg-gray-500 bg-opacity-50">
+                <form class="p-3 w-3/4 grid grid-cols-1 gap-1 flex items-center justify-center">
+                  
+                        <input type="text" name="search-address" id="search-address" class="py-2 my-2 rounded-sm p-1"
+                               placeholder="Search an address">
+                   
+                  
+                        <input type="text" name="search-city" id="search-city" class="py-2 my-2 rounded-sm p-1"
+                               placeholder="City">
+                   
+                    
+                        <input type="text" name="state" id="search-state" class="py-2 my-2 rounded-sm p-1"
+                               placeholder="Select your state">
+              
+                    
+                        <input type="text" name="search-zip" id="search-zip" class="py-2 my-2 rounded-sm p-1"
+                               placeholder="Zipcode">
+                 
+                </form>
             </div>
         </div>
     `
