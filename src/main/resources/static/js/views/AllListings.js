ae12a4a2cf418fb111e529b60f5d18398e97040d
Collin Jones <collin.g.jones34@gmail.com>
2022-05-31 09:24:39 -0500

    probably not a good idea to push this up but oh well

--- src/main/resources/static/js/views/AllListings.js
+++ src/main/resources/static/js/views/AllListings.js
@@ -1,16 +1,19 @@
 import createView from "../createView.js";
 
-export default function AllListings(props){
+export default function AllListings(props) {
     console.log(props)
     //language=HTML
     return props.allListings.map(listing => `
         <div id="map" class="h-[200px] w-full"></div>
-        <div class="w-11/12 flex flex-wrap justify-evenly border-2 border-callToAction bg-secondary m-2" data-id="${listing.id}" id="listings">
+        <div class="w-11/12 flex flex-wrap justify-evenly border-2 border-callToAction bg-secondary m-2"
+             data-id="${listing.id}" id="listings">
             <div class="m-1 pb-1 text-center">${listing.askingPrice}</div>
             <div class="m-1 pb-1 text-center" id="listing#-${listing.id}">MLS# ${listing.id}</div>
             <div class="m-1 pb-1 text-center">${listing.listingStatus}</div>
             <div class="m-1 pb-1 text-center">${listing.listingAddress.address}</div>
-            <div class="m-1 pb-1 text-center">${listing.listingAddress.city}, ${listing.listingAddress.state}, ${listing.listingAddress.zipCode}</div>
+            <div class="m-1 pb-1 text-center">${listing.listingAddress.city}, ${listing.listingAddress.state},
+                ${listing.listingAddress.zipCode}
+            </div>
             <div class="m-1 pb-1 text-center">${listing.sellerAgent.firstName} ${listing.sellerAgent.lastName}</div>
             <div class="m-1 pb-1 text-center">${listing.sellerAgent.email}</div>
             <p class="w-full text-justify">${listing.description}</p>
@@ -29,35 +32,48 @@ export function AllListingsEvent() {
     initMap();
     codeAddress(document.getElementById('address').value);
 }
-    function initMap() {
-        const options = {
-            //map options
-            zoom: 8,
-            center: {lat: 32.7767, lng: -96.7970}
-        }
-        //New map
-        const map = new
-        google.maps.Map(document.getElementById('map'), options);
 
-        addMarker({coords: {lat: 32.7767 , lng: -96.7970}});
-        function addMarker(props) {
-            const marker = new google.maps.Marker({
-                position: props.coords,
-                map: map
-            });
-        }
+function initMap() {
+    const options = {
+        //map options
+        zoom: 8,
+        center: {lat: 32.7767, lng: -96.7970}
+    }
+    //New map
+    const map = new
+    google.maps.Map(document.getElementById('map'), options);
+
+    addMarker({coords: {lat: 32.7767, lng: -96.7970}});
+
+    function addMarker(props) {
+        const marker = new google.maps.Marker({
+            position: props.coords,
+            map: map
+        });
+    }
 }
 
 function codeAddress(address) {
-    geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status === 'OK') {
-            map.setCenter(results[0].geometry.location);
+        for (address = 0; address < locations.length; address++) {
             const marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
+                position: new google.maps.LatLng(locations[count][i], locations[count][i]),
+                map: this.map,
+                title: locations[count][i]
             });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
         }
-    });
 }
+
+
+// function codeAddress(address) {
+//     geocoder.geocode( { 'address': address}, function(results, status) {
+//         if (status === 'OK') {
+//             map.setCenter(results[0].geometry.location);
+//             const marker = new google.maps.Marker({
+//                 map: map,
+//                 position: results[0].geometry.location
+//             });
+//         } else {
+//             alert('Geocode was not successful for the following reason: ' + status);
+//         }
+//     });
+// }
