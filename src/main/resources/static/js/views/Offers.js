c81cd03e65d1e147e7cca0cb2d0fc987e26a6be1
Scott Carpenter <carpenter.scott22@gmail.com>
2022-05-30 22:17:56 -0500

    successfully rendered button show/hide to users

--- src/main/resources/static/js/views/Offers.js
+++ src/main/resources/static/js/views/Offers.js
@@ -6,33 +6,29 @@ const OFFERS_URL = `http://${BACKEND_HOST}:${PORT}/api/offers`;
 let listingId = null;
 
 const user = parseInt(localStorage.getItem('accessToken'));
-let isRealtor;
-let isSeller;
+
 
 export default function Offers(props) {
-    // console.log(props)
+    console.log(props.offers);
     let URI = sessionStorage.getItem("URI").split("/")
-    // console.log(URI)
     listingId = parseInt(URI[URI.length - 1])
+
     let userId = parseInt(localStorage.getItem("accessToken"));
-    // console.log(props.offers[0].offeror.id)
-    const makeOfferBtn = buttonAuth(props.offers, userId);
-    //  console.log(makeOfferBtn);
+
+    // const makeOfferBtn = buttonAuth(props.offers, userId);
+    buttonAuthorization(props.offers, userId, listingId);
     //language=HTML
-    disableMakeOfferBtn(makeOfferBtn);
+    // disableMakeOfferBtn(makeOfferBtn);
     return `
         <div class="min-h-[calc(100vh-90px)] bg-primary">
             <div class="w-full relative">
                 <img class="md:w-3/4 md:h-[350px] mx-auto"
                      src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
                      alt="main listing photo">
-
                 <button id="makeOfferBtn"
                         class=" absolute top-[50%] right-[50%] translate-y-1/2 translate-x-1/2 p-2 mx-1 my-2 rounded-md shadow-xl text-white bg-callToAction">
                     Make an offer!
                 </button>
-
-
             </div>
             <div id="offer">
                 ${props.offers.length === 0 ? `<h1>Currently No Offers Submitted</h1>` : retrieveOffersFromDb(props.offers)}
@@ -254,9 +250,7 @@ const createMakeOfferView = () => {
         console.log(URI)
         listingId = parseInt(URI[URI.length - 1])
         console.log(listingId)
-
         createView(`/makeOffer/listings/${listingId}`)
-
     })
 }
 
@@ -282,6 +276,30 @@ function disableMakeOfferBtn(auth) {
     }
 }
 
+function buttonAuthorization(offers, userId, listingId) {
+    let sellerId = parseInt(offers[0].listing.seller.id);
+    let currentOfferorId = null;
+
+    offers.forEach(function (offer) {
+        let searchOfferors = parseInt(offer.offeror.id)
+        if (searchOfferors === userId) {
+            currentOfferorId = userId;
+        }
+    })
+
+        $(document).ready(function () {
+            if (sellerId === userId) {
+                $('#makeOfferBtn').hide();
+            }
+
+            if (currentOfferorId !== null) {
+                $('#makeOfferBtn').hide();
+                $('.btn-accept').hide();
+                $('.btn-counter').hide();
+                $('#editOfferBtn').show();
+            }
+        })
+}
 export function OfferEvent() {
     confirmOfferAcceptance();
     updateListingObject();
