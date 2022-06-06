d2e52c8be980f069938b11bb9cf30fb5233bef02
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-06 11:17:48 -0500

    adding items to footer

--- src/main/resources/static/js/views/partials/Footer.js
+++ src/main/resources/static/js/views/partials/Footer.js
@@ -1,7 +1,25 @@
-
 export default function Footer(props) {
     //language=HTML
     return `
-        <footer class="w-full h-[50px] bottom-0 bg-primary">THIS IS THE FOOTER</footer>
+        <footer class="w-full h-[80px] bottom-0 bg-primary">
+            <div class="container-fluid flex justify-center">
+                <ul class="flex justify-center items-center text-">
+                    <li>
+                        <a class="nav-link" href="/realtorListing" data-link>About</a>
+                    </li>
+                    <li>
+                        <a class="navbar-brand" href="/" data-link>
+                            <img src="../../../img/logo.png"
+                                 alt="Clear to Close Logo">
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="/realtorListing" data-link>Contact Us</a>
+                    </li>
+                </ul>
+<!--                <br>-->
+<!--                <span>Copyright© 2022, Clear to Close</span>-->
+            </div>
+        </footer>
     `
 }
